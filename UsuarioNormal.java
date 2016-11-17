611ce38615fd878465f397825affd10ca4d633ce
felipe11dias <clame1997mac@hotmail.com>
2016-11-17 13:41:07 -0200

    Update UsuarioNormal.java

--- UsuarioNormal.java
+++ UsuarioNormal.java
@@ -5,19 +5,28 @@ public class UsuarioNormal extends Usuario {
 	protected int tel;
 	protected String email;
 	protected String end;
+	protected int cpf;
 	protected int limiteItensAlugados;
 	protected int limiteDias;
 	
 	
-	public UsuarioNormal(String nome, int tel, String email, String end, int limiteItensAlugados, int limiteDias) {
-		super();
+	public UsuarioNormal(String nome, int tel, String email, String end, int limiteItensAlugados, int limiteDias,int cpf,int codigo,String senha) {
+		super(codigo, senha);
 		this.nome = nome;
 		this.tel = tel;
 		this.email = email;
 		this.end = end;
+		this.cpf = cpf;
 		this.limiteItensAlugados = limiteItensAlugados;
 		this.limiteDias = limiteDias;
 	}
-	
+
+
+	public int getLimiteItensAlugados() {
+		return limiteItensAlugados;
+	}
+	public int getLimiteDias() {
+		return limiteDias;
+	}
 	
 }
