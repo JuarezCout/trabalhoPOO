1f525ff4c80086ae31caf35a9de910d627cf805a
felipe11dias <clame1997mac@hotmail.com>
2016-11-17 23:19:15 -0200

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,5 +1,30 @@
 package bibliotecaDigital;
 
-public class Item {
-
+public class Itens {
+	
+	// Codigos : 1 - Livros , 2 - Revistas e 3 - Periodicos .
+	
+	protected String titulo;
+	protected int codigo;
+	protected int dataEmprestimo;
+	protected int entregaReal;
+	protected int entregaPrevista;
+	
+	public Itens(String titulo, int codigo, int dataEmprestimo, int entregaReal, int entregaPrevista) {
+		super();
+		this.titulo = titulo;
+		this.codigo = codigo;
+		this.dataEmprestimo = dataEmprestimo;
+		this.entregaReal = entregaReal;
+		this.entregaPrevista = dataEmprestimo + 5;
+	}
+	
+	public static double calcularMulta(int diasExcedidos){
+		return diasExcedidos * 1.50;
+	}
+	
+	public static void calcularDiaEntrega(int entregaPrevista){
+		System.out.println("Dia de entrega :" + entregaPrevista);
+	}
 }
+
